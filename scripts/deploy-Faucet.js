4876f4e76a5a2fceff67fa09edb345e91abbf5d9
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-16 11:18:31 +0200

    contracts name updated

--- scripts/deploy-Faucet.js
+++ scripts/deploy-Faucet.js
@@ -16,15 +16,15 @@ async function main() {
   console.log('Deploying contracts with the account:', deployer.address);
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
+  const Faucet = await hre.ethers.getContractFactory('Faucet');
+  const faucet = await Faucet.deploy('Hello, Hardhat!');
 
   // Attendre que le contrat soit réellement déployé, cad que la transaction de déploiement
   // soit incluse dans un bloc
-  await greeter.deployed();
+  await faucet.deployed();
 
   // Create/update deployed.json and print usefull information on the console.
-  await deployed('Greeter', hre.network.name, greeter.address);
+  await deployed('Faucet', hre.network.name, faucet.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
