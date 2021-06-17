231378dc71282a873a21966125e47616619e49a5
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-17 14:08:17 +0200

    fix

--- scripts/deploy-Faucet.js
+++ scripts/deploy-Faucet.js
@@ -1,12 +1,14 @@
 /* eslint-disable space-before-function-paren */
 /* eslint-disable no-undef */
-const { readFile, writeFile } = require('fs/promises');
+const { readFile } = require('fs/promises');
 const hre = require('hardhat');
 const { deployed } = require('./deployed');
 const FAUCET_NAME = 'Faucet';
 const FILE_PATH = './deployed.json';
 
 const checkDeploy = async () => {
+  let jsonString = ""
+  let obj= {}
   try {
     jsonString = await readFile(FILE_PATH, 'utf-8');
     obj = JSON.parse(jsonString);
